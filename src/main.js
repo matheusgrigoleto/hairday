837582eb2feef6f183ffc9cae0aa9c1f87900176
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-13 15:36:00 -0300

    All dependencies installed

---
+++ src/main.js
@@ -0,0 +1,12 @@
+"use strict"
+
+// Configuração do dayjs
+import "./libs/dayjs.js"
+
+// CSS
+import "./styles/global.css"
+import "./styles/form.css"
+import "./styles/schedule.css"
+
+import dayjs from "dayjs"
+console.log(dayjs().format("DD/MM - HH:mm"))
